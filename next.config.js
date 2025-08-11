5641dbeffa3213ceb57cd95475b26f2d03616556
saurabh <saurabhmadhure364@gmail.com>
2025-08-11 09:54:35 +0530

    added deployment file

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/instagram-dummy-ui',
+    assetPrefix: '/instagram-dummy-ui/'
+  };
+  
+  module.exports = nextConfig;
+  
